62f452ff5b5386c3a71178a9f211a719d442e1f0
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-02-21 05:39:52 +0300

    Add a class Credentials to save credentials for users

--- user.py
+++ user.py
@@ -2,3 +2,23 @@ class User:
     '''
     Class that generates new instances of a user.
     '''
+
+    user_list = []
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+
+class Credentials:
+    '''
+    Class that generates credentials for each user.
+    '''
+
+    credentials_list = []
+
+    def __init__(self, account, userName, password):
+        self.account = account
+        self.userNmae = username
+        self.password = password
